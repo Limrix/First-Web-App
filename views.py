605e1207c75c719220e9abe07c022178914c9725
Limrix <mattymg95@live.co.uk>
2024-06-17 14:45:18 +0100

    Corrected some Def issues with pages and corrected code

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from fav_bands import favourite_bands
 
 my_view = Blueprint("my_view", __name__)
@@ -8,10 +8,13 @@ def index():
     return render_template("index.html")
 
 @my_view.route("/page2")
-def about():
+def page2():
     return render_template("page2.html", favourite_bands = favourite_bands)
 
-@my_view.route("/my_name")
-def contact():
-    my_name = "Matthew"
-    return render_template("page3.html", my_name = my_name)
+@my_view.route("/page3", methods=["GET", "POST"])
+def page3():
+    if request.method =="POST":
+        new_band = request.form["added_band"]
+        favourite_bands.append(new_band)
+
+    return render_template("page3.html")
